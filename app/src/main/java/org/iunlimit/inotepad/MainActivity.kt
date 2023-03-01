4f00ed95e932e6bafd11aaf1d26375d63ff23083
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-03-01 22:45:44 +0800

    Feat: Add basic components

--- app/src/main/java/org/iunlimit/inotepad/MainActivity.kt
+++ app/src/main/java/org/iunlimit/inotepad/MainActivity.kt
@@ -3,11 +3,28 @@ package org.iunlimit.inotepad
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("test", "6666666")
+
+        setupActionBarWithNavController(supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment)!!.findNavController())
+
     }
+
+    override fun onNavigateUp(): Boolean {
+        Log.d("Inotepad", "onNavigateUp")
+//        val navController = findNavController(R.id.nav_host_fragment)
+        val navController = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment)!!.findNavController()
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
---
+++ app/src/main/java/org/iunlimit/inotepad/fragments/list/ListFragment.kt
@@ -0,0 +1,35 @@
+package org.iunlimit.inotepad.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.iunlimit.inotepad.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        // FloatingActionButton click listener
+        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        // List click listener
+        view.findViewById<ConstraintLayout>(R.id.list_layout).setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        return view
+    }
+
+}
