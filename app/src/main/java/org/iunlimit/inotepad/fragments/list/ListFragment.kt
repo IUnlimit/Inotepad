5e09b4d1d607728cfe7083c03eb6db6aea6ca127
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-03-23 23:56:11 +0800

    Feat: Add file selector

--- app/src/main/java/org/iunlimit/inotepad/fragments/list/ListFragment.kt
+++ app/src/main/java/org/iunlimit/inotepad/fragments/list/ListFragment.kt
@@ -1,7 +1,6 @@
 package org.iunlimit.inotepad.fragments.list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,17 +8,26 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.input.getInputField
 import com.afollestad.materialdialogs.input.input
+import com.molihuan.pathselector.PathSelector
+import com.molihuan.pathselector.entity.FileBean
+import com.molihuan.pathselector.fragment.BasePathSelectFragment
+import com.molihuan.pathselector.listener.FileItemListener
+import com.molihuan.pathselector.utils.MConstants
+import com.molihuan.pathselector.utils.Mtools
 import org.iunlimit.inotepad.R
 import org.iunlimit.inotepad.data.viewmodel.FileViewModel
 import org.iunlimit.inotepad.databinding.FragmentListBinding
 import org.iunlimit.inotepad.fragments.SharedViewModel
 import org.iunlimit.inotepad.fragments.list.adapter.ListAdapter
+import org.iunlimit.inotepad.fragments.setFont
 import org.iunlimit.inotepad.fragments.update.UpdateFragment
+import java.io.File
+
 
 class ListFragment : Fragment() {
 
@@ -49,6 +57,29 @@ class ListFragment : Fragment() {
             searchData()
         }
 
+        binding.menuImport.setOnClickListener {
+            //如果没有权限会自动申请权限
+            PathSelector.build(this, MConstants.BUILD_DIALOG)
+                .setShowTitlebarFragment(false)
+                .setFileItemListener(//设置文件item点击回调(点击是文件才会回调,如果点击是文件夹则不会)
+                    object : FileItemListener() {
+                        override fun onClick(
+                            v: View?,
+                            file: FileBean?,
+                            currentPath: String?,
+                            fragment: BasePathSelectFragment?
+                        ): Boolean {
+                            fragment?.dismiss()
+                            val filePath = file?.path ?: return false
+                            importFile(filePath)
+                            Mtools.toast("select file:\n$filePath")
+                            return false
+                        }
+                    }
+                )
+                .show()
+        }
+
         // observe LiveData
         viewModel.allData.observe(viewLifecycleOwner) { data ->
             sharedViewModel.checkDatabaseEmpty(data)
@@ -56,6 +87,11 @@ class ListFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setFont(requireView().findViewById(R.id.no_data_textView))
     }
 
     // avoid memory release
@@ -67,7 +103,7 @@ class ListFragment : Fragment() {
     private fun setupRecyclerView() {
         val recyclerView = binding.recyclerView
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        recyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
         // BUG 不响应 notifyItemChanged
 //        recyclerView.itemAnimator = SlideInUpAnimator().apply {
@@ -76,6 +112,11 @@ class ListFragment : Fragment() {
 
         // Swipe to delete
         swipeToDelete(recyclerView)
+    }
+
+    private fun importFile(filePath: String) {
+        val file = File(filePath)
+        viewModel.insertData(file)
     }
 
     private fun swipeToDelete(recyclerView: RecyclerView) {
@@ -101,10 +142,20 @@ class ListFragment : Fragment() {
             negativeButton(R.string.cancel)
             positiveButton(R.string.confirm) {
                 val text = dialog.getInputField().text
-            Log.d("Inotepad", "=========================${text}")
-//            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+                searchThroughDatabase(text.toString())
             }
         }
     }
+
+    private fun searchThroughDatabase(content: String) {
+        val query = "%$content%"
+        viewModel.searchData(query).observe(viewLifecycleOwner) {
+            it.let {
+                adapter.setData(it)
+            }
+        }
+    }
+
+
 
 }
