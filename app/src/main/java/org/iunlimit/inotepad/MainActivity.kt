78cc81416c183864551ccd82f9d03b9b652a8d28
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-03-24 00:00:38 +0800

    Feat: Update resource

--- app/src/main/java/org/iunlimit/inotepad/MainActivity.kt
+++ app/src/main/java/org/iunlimit/inotepad/MainActivity.kt
@@ -1,13 +1,8 @@
 package org.iunlimit.inotepad
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
