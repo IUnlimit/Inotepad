2314ba3aaeb0d2959b546cc3902716b990843c00
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-04-02 00:43:37 +0800

    Update: Add webView preview fragment

---
+++ app/src/main/java/org/iunlimit/inotepad/fragments/preview/PreviewWebViewClient.kt
@@ -0,0 +1,48 @@
+package org.iunlimit.inotepad.fragments.preview
+
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class PreviewWebViewClient(
+    val fragment: WebViewFragment
+) : WebViewClient() {
+
+    /**
+     * 覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+     * */
+    override fun shouldOverrideUrlLoading(
+        view: WebView?,
+        request: WebResourceRequest?
+    ): Boolean {
+        val url = request!!.url.toString()
+        Log.v("webView", "load url: $url")
+        if (url.startsWith("http:") || url.startsWith("https:") || url.startsWith("ftp")) {
+            view!!.loadUrl(url)
+            return true
+        }
+        if (url.startsWith("scheme://")) {
+            // 使用浏览器打开
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            fragment.startActivity(intent)
+            return true
+        }
+        return false
+    }
+
+    /**
+     * 在vue页面加载后进行调用
+     * */
+    override fun onPageFinished(view: WebView?, url: String?) {
+        // 在 onPageFinished 回调里调用，表示页面加载好就调用
+        view!!.post {
+            view.evaluateJavascript("javascript:setData(`${fragment.args.content}`, '${fragment.args.type.value.substring(1)}')") {
+                Log.d("MainActivity", "js返回的结果： $it")
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/org/iunlimit/inotepad/fragments/preview/WebViewFragment.kt
@@ -0,0 +1,50 @@
+package org.iunlimit.inotepad.fragments.preview
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
+import org.iunlimit.inotepad.R
+
+// 图片 -> vue#Image
+// code -> vue#Code
+// office -> vue#Office
+class WebViewFragment : Fragment() {
+
+    val args by navArgs<WebViewFragmentArgs>()
+
+    @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.webview, container, false)
+        val webView = view.findViewById<WebView>(R.id.webView)
+
+        webView.settings.allowFileAccess = true // allow cros
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportMultipleWindows(true)
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.domStorageEnabled = true // 是否启用本地存储 (localStorage
+//        webView.settings.builtInZoomControls = true // 是否应使用其内置的缩放机制
+
+        webView.webViewClient = PreviewWebViewClient(this)
+
+        webView.loadUrl("file:///android_asset/code/dist/index.html")
+        return view
+    }
+
+}
