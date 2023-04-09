1cd4d8c3f00e5676096ba3736753aa1196ec101a
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-04-09 13:25:38 +0800

    Feat: Use async method replace coroutine

--- app/src/main/java/org/iunlimit/inotepad/fragments/preview/handler/OfficeTypeHandler.kt
+++ app/src/main/java/org/iunlimit/inotepad/fragments/preview/handler/OfficeTypeHandler.kt
@@ -5,30 +5,27 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.webkit.ValueCallback
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.iunlimit.inotepad.data.models.FileData
-import org.iunlimit.inotepad.util.OSSHolder
+import org.iunlimit.inotepad.sdk.OSSHolder
+import org.iunlimit.inotepad.sdk.sts
 import java.util.*
 
 class OfficeTypeHandler(val context: Context): TypeHandler {
 
-    @OptIn(DelicateCoroutinesApi::class)
     override fun invoke(
         fileData: FileData,
         eval: (script: String, resultCallback: ValueCallback<String>?) -> Unit
     ) {
         assert(fileData.filePath != null)
 
-        GlobalScope.launch {
-            val oss = OSSHolder(context)
-            val uuid = UUID.randomUUID().toString()
-            val url = oss.upload(uuid + fileData.type.value, fileData.filePath!!)
-            Log.v("handler#office", "Upload to $url")
+        val oss = OSSHolder(context, sts)
+        val uuid = UUID.randomUUID().toString()
+
+        oss.asyncUpload(uuid + fileData.type.value, fileData.filePath!!) {
+            Log.v("handler#office", "Upload to $it")
             // main thread
             Handler(Looper.getMainLooper()).post{
-                eval("javascript:setData(`${url}`, '${fileData.type.value.substring(1)}')") { result ->
+                eval("javascript:setData(`${it}`, '${fileData.type.value.substring(1)}')") { result ->
                     Log.v("handler#office", result)
                     // TODO delete file after preview
 //                    val delete = oss.delete(uuid + fileData.type.value)
