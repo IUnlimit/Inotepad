faf05f4b571ae52b0fb70e0f8e298e49f738cb57
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-04-10 00:08:03 +0800

    Feat: Add text preview support

--- app/src/main/java/org/iunlimit/inotepad/fragments/preview/handler/TxtTypeHandler.kt
+++ app/src/main/java/org/iunlimit/inotepad/fragments/preview/handler/TxtTypeHandler.kt
@@ -1,4 +1,20 @@
 package org.iunlimit.inotepad.fragments.preview.handler
 
-class TxtTypeHandler {
+import android.util.Log
+import android.webkit.ValueCallback
+import org.iunlimit.inotepad.data.models.FileData
+
+object TxtTypeHandler: TypeHandler {
+    override fun invoke(
+        fileData: FileData,
+        eval: (script: String, resultCallback: ValueCallback<String>?) -> Unit
+    ) {
+        eval("javascript:setData(`${fileData.content.replace("`", "\\`")}`)") {
+            Log.v("handler#txt", it)
+        }
+    }
+
+    override fun getName(): String {
+        return "txt"
+    }
 }
